7d08e6c4ee58f45a453f2821069810fda890b6af
Benito Matteo Bercini <bercini.matteo@gmail.com>
2025-06-09 22:50:54 +0200

    env.dart: add Cloudinary secrets

--- lib/config/env/env.dart
+++ lib/config/env/env.dart
@@ -13,4 +13,13 @@ final class Env {
 
   @EnviedField(varName: 'SENTRY_URL', obfuscate: true)
   static final String sentryUrl = _Env.sentryUrl;
+
+  @EnviedField(varName: 'CLOUDINARY_PROD_CLOUD_NAME', obfuscate: true)
+  static final String cloudinaryProdCloudName = _Env.cloudinaryProdCloudName;
+
+  @EnviedField(varName: 'CLOUDINARY_PROD_API_KEY', obfuscate: true)
+  static final String cloudinaryProdApiKey = _Env.cloudinaryProdApiKey;
+
+  @EnviedField(varName: 'CLOUDINARY_PROD_API_SECRET', obfuscate: true)
+  static final String cloudinaryProdApiSecret = _Env.cloudinaryProdApiSecret;
 }
