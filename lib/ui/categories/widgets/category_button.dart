7a7fa495c704d6928d4f7c5cc60aef0a2e8b4c01
Benito Matteo Bercini <bercini.matteo@gmail.com>
2025-06-20 14:16:09 +0200

    category_button.dart: fix for color.dart changes, simplify logic

--- lib/ui/categories/widgets/category_button.dart
+++ lib/ui/categories/widgets/category_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:moliseis/domain/models/attraction/attraction_type.dart';
 import 'package:moliseis/ui/core/themes/color.dart';
-import 'package:moliseis/ui/core/themes/shapes.dart';
+import 'package:moliseis/ui/core/themes/shape.dart';
 import 'package:moliseis/utils/extensions.dart';
 
 class CategoryButton extends StatelessWidget {
@@ -16,47 +16,23 @@ class CategoryButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final p = theme.colorScheme.primary;
-    final b = theme.brightness;
-    final Color? bgColor;
-    final Color? fgColor;
-
-    switch (attractionType) {
-      case AttractionType.unknown:
-        bgColor = null;
-        fgColor = null;
-      case AttractionType.nature:
-        bgColor = CustomColorSchemes.nature(p, b).surfaceContainerLow;
-        fgColor = CustomColorSchemes.nature(p, b).primary;
-      case AttractionType.history:
-        bgColor = CustomColorSchemes.history(p, b).surfaceContainerLow;
-        fgColor = CustomColorSchemes.history(p, b).primary;
-      case AttractionType.folklore:
-        bgColor = CustomColorSchemes.folklore(p, b).surfaceContainerLow;
-        fgColor = CustomColorSchemes.folklore(p, b).primary;
-      case AttractionType.food:
-        bgColor = CustomColorSchemes.food(p, b).surfaceContainerLow;
-        fgColor = CustomColorSchemes.food(p, b).primary;
-      case AttractionType.allure:
-        bgColor = CustomColorSchemes.allure(p, b).surfaceContainerLow;
-        fgColor = CustomColorSchemes.allure(p, b).primary;
-      case AttractionType.experience:
-        bgColor = CustomColorSchemes.experience(p, b).surfaceContainerLow;
-        fgColor = CustomColorSchemes.experience(p, b).primary;
-    }
-
-    return ElevatedButton.icon(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: bgColor,
-        foregroundColor: fgColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(Shapes.medium),
+    return Theme(
+      data: Theme.of(context).copyWith(
+        colorScheme: CustomColorSchemes.fromAttractionType(
+          attractionType,
+          Theme.of(context).brightness,
         ),
       ),
-      icon: Icon(attractionType.iconAlt),
-      label: Text(attractionType.label),
+      child: ElevatedButton.icon(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(Shape.medium),
+          ),
+        ),
+        icon: Icon(attractionType.iconAlt),
+        label: Text(attractionType.label),
+      ),
     );
   }
 }
