fd3bbe3de7836a9d962610c256e4e88c5d6602d2
Benito Matteo Bercini <xmattjus@gmail.com>
2026-02-07 14:53:03 +0100

    feat(objectbox): declare an app group to run the app in a sandboxed env on macOS

--- lib/data/services/objectbox.dart
+++ lib/data/services/objectbox.dart
@@ -1,3 +1,5 @@
+import 'dart:io' show Platform;
+
 import 'package:logging/logging.dart';
 import 'package:moliseis/generated/objectbox.g.dart';
 import 'package:path/path.dart' as p;
@@ -9,8 +11,14 @@ class ObjectBox {
   ObjectBox._create(this.store);
 
   static Future<ObjectBox> create() async {
+    // Declare an app group name to run the app in a sandboxed environment on macOS.
+    // See https://pub.dev/documentation/objectbox/latest/objectbox/Store/Store.html#:~:text=Sandboxed%20macOS%20apps
+    final macOSAppGroup = Platform.isMacOS ? 'group.XIPB6vBUQJblN' : null;
     final docsDir = await getApplicationDocumentsDirectory();
-    final store = await openStore(directory: p.join(docsDir.path, 'db_v2'));
+    final store = await openStore(
+      directory: p.join(docsDir.path, 'db_v2'),
+      macosApplicationGroup: macOSAppGroup,
+    );
     return ObjectBox._create(store);
   }
 }
