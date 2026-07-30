97861ddc5c1ef16b566633c6bf82270b46a0ff38
Benito Matteo Bercini <xmattjus@gmail.com>
2026-07-30 19:20:16 +0200

    refactor(env): reorder fields and add dartdoc comments

--- lib/config/env/env.dart
+++ lib/config/env/env.dart
@@ -2,24 +2,30 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
-// ignore: avoid_classes_with_only_static_members
+/// A class containing the app environment variables.
 @Envied(path: '.env')
 final class Env {
-  @EnviedField(varName: 'SUPABASE_PROD_URL', obfuscate: true)
-  static final String supabaseProdUrl = _Env.supabaseProdUrl;
-
-  @EnviedField(varName: 'SUPABASE_PROD_KEY', obfuscate: true)
-  static final String supabaseProdApiKey = _Env.supabaseProdApiKey;
-
-  @EnviedField(varName: 'SENTRY_URL', obfuscate: true)
-  static final String sentryUrl = _Env.sentryUrl;
-
-  @EnviedField(varName: 'CLOUDINARY_PROD_CLOUD_NAME', obfuscate: true)
-  static final String cloudinaryProdCloudName = _Env.cloudinaryProdCloudName;
-
+  /// The Cloudinary production instance key.
   @EnviedField(varName: 'CLOUDINARY_PROD_API_KEY', obfuscate: true)
   static final String cloudinaryProdApiKey = _Env.cloudinaryProdApiKey;
 
+  /// The Cloudinary production instance secret.
   @EnviedField(varName: 'CLOUDINARY_PROD_API_SECRET', obfuscate: true)
   static final String cloudinaryProdApiSecret = _Env.cloudinaryProdApiSecret;
+
+  /// The Cloudinary production instance URL.
+  @EnviedField(varName: 'CLOUDINARY_PROD_CLOUD_NAME', obfuscate: true)
+  static final String cloudinaryProdCloudName = _Env.cloudinaryProdCloudName;
+
+  /// The Sentry instance URL.
+  @EnviedField(varName: 'SENTRY_URL', obfuscate: true)
+  static final String sentryUrl = _Env.sentryUrl;
+
+  /// The Supabase production instance key.
+  @EnviedField(varName: 'SUPABASE_PROD_KEY', obfuscate: true)
+  static final String supabaseProdApiKey = _Env.supabaseProdApiKey;
+
+  /// The Supabase production instance URL.
+  @EnviedField(varName: 'SUPABASE_PROD_URL', obfuscate: true)
+  static final String supabaseProdUrl = _Env.supabaseProdUrl;
 }
