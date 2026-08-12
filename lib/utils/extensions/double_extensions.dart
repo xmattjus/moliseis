7bdcdeb4f1267a637b09e99638351ad190fa95a5
Benito Matteo Bercini <xmattjus@gmail.com>
2026-08-12 18:45:13 +0200

    chore(utils): add messages to toLatLng asserts
    State the two-element shape and valid latitude/longitude ranges in
    the asserts' messages, including the offending value.

--- lib/utils/extensions/double_extensions.dart
+++ lib/utils/extensions/double_extensions.dart
@@ -5,9 +5,18 @@ extension ListDoubleExtensions on List<double> {
   /// The list must contain exactly two elements:
   /// the first element is treated as latitude and the second as longitude.
   LatLng get toLatLng {
-    assert(length == 2);
-    assert(this[0] >= -90 && this[0] <= 90);
-    assert(this[1] >= -180 && this[1] <= 180);
+    assert(
+      length == 2,
+      'A LatLng requires exactly two values: latitude and longitude.',
+    );
+    assert(
+      this[0] >= -90 && this[0] <= 90,
+      'Latitude must be between -90 and 90 degrees; got ${this[0]}.',
+    );
+    assert(
+      this[1] >= -180 && this[1] <= 180,
+      'Longitude must be between -180 and 180 degrees; got ${this[1]}.',
+    );
 
     return LatLng(this[0], this[1]);
   }
